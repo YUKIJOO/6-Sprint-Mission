f955ee05f3049d9e203d724bb094f516eb5a3272
YUKIJOO <koreala1@naver.com>
2024-04-15 19:41:49 +0900

    "sprintmission5- 헤더 작업"

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./header.css";
+
+<header>
+  <a href="index.html" aria-label="홈으로 이동">
+    <img src="images/logo/logo.svg" alt="판다마켓 로고" width="153" />
+  </a>
+  <a id="freeAnnounce">자유게시판</a>
+  <a id="usedMarket">중고마켓</a>
+  <a href="login.html" id="loginLink" className="button">
+    로그인
+  </a>
+</header>;
