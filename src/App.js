9b60b2e670afaa40b8975bf825cc46b11be3ca8d
YUKIJOO <koreala1@naver.com>
2024-04-16 21:59:18 +0900

    "중고마켓 페이지 주소설정"

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import "./header.css";
+import { Link } from "react-router-dom";
 
 <header>
   <a href="index.html" aria-label="홈으로 이동">
     <img src="images/logo/logo.svg" alt="판다마켓 로고" width="153" />
   </a>
   <a id="freeAnnounce">자유게시판</a>
-  <a id="usedMarket">중고마켓</a>
+  <Link to="/items" id="usedMarket">
+    중고마켓
+  </Link>
   <a href="login.html" id="loginLink" className="button">
     로그인
   </a>
