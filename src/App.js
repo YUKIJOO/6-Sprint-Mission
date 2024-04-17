6ea3c320aed42294be2deb21426a92212e2ce1dc
YUKIJOO <koreala1@naver.com>
2024-04-17 21:00:26 +0900

    "백엔드 api에서 데이터 받아오기"

--- src/App.js
+++ src/App.js
@@ -3,13 +3,24 @@ import { BrowserRouter, Route, Switch } from "react-router-dom";
 import { freewriter, login, market, pandamarket, Nav } from "./pages";
 
 function App() {
+  async function getData() {
+    try {
+      const response = await axios.get(
+        "https://panda-market-api.vercel.app/docs/#/"
+      );
+      console.log(response);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
   return (
     <BrowserRouter>
       <Nav />
       <Switch>
         <Route exact path="/pandamarket" component={pandamarket} />
         <Route exact path="/freewirter" component={freewriter} />
-        <Route exact path="/usedmarket" component={market} />
+        <Route exact path="/items" component={market} />
         <Route exact path="/login" component={login} />
       </Switch>
     </BrowserRouter>
