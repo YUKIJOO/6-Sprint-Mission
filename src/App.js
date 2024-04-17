e56953c9e6bbcf48ebf60db63af49e74e67aafc1
YUKIJOO <koreala1@naver.com>
2024-04-17 20:48:25 +0900

    "헤더 부분 라우터 login 부분 수정"

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
-import "./header.css";
-import { Link } from "react-router-dom";
+import React from "react";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { freewriter, login, market, pandamarket, Nav } from "./pages";
 
-<header>
-  <a href="index.html" aria-label="홈으로 이동">
-    <img src="images/logo/logo.svg" alt="판다마켓 로고" width="153" />
-  </a>
-  <a id="freeAnnounce">자유게시판</a>
-  <Link to="/items" id="usedMarket">
-    중고마켓
-  </Link>
-  <a href="login.html" id="loginLink" className="button">
-    로그인
-  </a>
-</header>;
+function App() {
+  return (
+    <BrowserRouter>
+      <Nav />
+      <Switch>
+        <Route exact path="/pandamarket" component={pandamarket} />
+        <Route exact path="/freewirter" component={freewriter} />
+        <Route exact path="/usedmarket" component={market} />
+        <Route exact path="/login" component={login} />
+      </Switch>
+    </BrowserRouter>
+  );
+}
+
+export default App;
