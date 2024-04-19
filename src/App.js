83527d62bb2eb4ebf11ae38a495e58d31627bed1
YUKIJOO <koreala1@naver.com>
2024-04-19 13:49:52 +0900

    "헤더 부분 수정 - Router, NavLink"

--- src/App.js
+++ src/App.js
@@ -1,28 +1,21 @@
 import React from "react";
-import { BrowserRouter, Route, Switch } from "react-router-dom";
-import { freewriter, login, market, pandamarket, Nav } from "./pages";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { freewriter, login, market, pandamarket } from "./pages";
+import Header from "./componenets/Header/Header";
 
 function App() {
-  async function getData() {
-    try {
-      const response = await axios.get(
-        "https://panda-market-api.vercel.app/docs/#/"
-      );
-      console.log(response);
-    } catch (error) {
-      console.error(error);
-    }
-  }
-
   return (
     <BrowserRouter>
-      <Nav />
-      <Switch>
-        <Route exact path="/pandamarket" component={pandamarket} />
-        <Route exact path="/freewirter" component={freewriter} />
-        <Route exact path="/items" component={market} />
-        <Route exact path="/login" component={login} />
-      </Switch>
+      <Header />
+
+      <div>
+        <Routes>
+          <Route exact path="/pandamarket" component={pandamarket} />
+          <Route exact path="/freewirter" component={freewriter} />
+          <Route exact path="/items" component={market} />
+          <Route exact path="/login" component={login} />
+        </Routes>
+      </div>
     </BrowserRouter>
   );
 }
