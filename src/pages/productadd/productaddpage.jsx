0834a55d32348b751bb00d27f83492aed10f34cc
YUKIJOO <koreala1@naver.com>
2024-04-19 20:10:41 +0900

    "상품등록학기 - 각 페이지 설정"

--- src/pages/productadd/productaddpage.jsx
+++ src/pages/productadd/productaddpage.jsx
@@ -1,4 +1,3 @@
-import React, { useRef, useState } from "react";
 import "./product.css";
 import Productname from "./productname";
 import Productintroduction from "./productintroduction";
@@ -6,55 +5,20 @@ import ProductPrice from "./productprice";
 import ProductTag from "./producttag";
 
 function productaddpage() {
-  const [product, setProduct] = useState({ image: "" });
-
-  const inputImgRef = useRef(null);
-
-  const handleImgChange = () => {
-    const file = inputImgRef.current.files[0];
-    if (!file) {
-      return;
-    }
-    const reader = new FileReader();
-    reader.readAsDataURL(file);
-    reader.onloadend = () => {
-      setProduct({ ...product, image: reader.result });
-    };
-  };
-
-  const onClickInput = () => {
-    inputImgRef.current.click();
-  };
-
   return (
     <>
-      <productImg>
-        <label htmlFor="image">상품이미지</label>
-        <ProductImgInputContainer>
-          <input
-            type="file"
-            id="image"
-            name="image"
-            accept=".jpg,.jepg,.png,.gif"
-            ref={inputImgRef}
-            onChange={handleImgChange}
-          />
-          <ProductImgInputBtn type="button" onClick={onClickInput}>
-            <img src={imgUploadBtn} alt="이미지 추가하기" />
-          </ProductImgInputBtn>
-          {product.image && (
-            <ProductImgPreview
-              src={product.image}
-              alt="상품 미리보기"
-              onClick={onClickInput}
-            />
-          )}
-        </ProductImgInputContainer>
-      </productImg>
-      <Productname />
-      <Productintroduction />
-      <ProductPrice />
-      <ProductTag />
+      <div>
+        <Productname />
+      </div>
+      <div>
+        <Productintroduction />
+      </div>
+      <div>
+        <ProductPrice />
+      </div>
+      <div>
+        <ProductTag />
+      </div>
     </>
   );
 }
