36038198e52405ad55fc58cba47426c08713df82
YUKIJOO <koreala1@naver.com>
2024-06-07 19:10:49 +0900

    sprintmission9-" 프로젝트 생성"

---
+++ mission9/components/Header.js
@@ -0,0 +1,41 @@
+import React from "react";
+import Logo from "../public/logo.svg";
+import { Link } from "react-router-dom";
+import "./Heaer.module.css";
+
+function getLinkStyle({ isActive }) {
+  return { color: isActive ? "var(--blue)" : undefined };
+}
+
+function Header() {
+  return (
+    <header className="globalHeader">
+      <div className="headerLeft">
+        <Link to="/" className="headerLogo" aria-label="홈으로 이동">
+          <img src={Logo} alt="판다마켓 로고" width="153" />
+        </Link>
+
+        <nav>
+          <ul>
+            <li>
+              <Link to="/community" style={getLinkStyle}>
+                자유게시판
+              </Link>
+            </li>
+            <li>
+              <Link to="/items" style={getLinkStyle}>
+                중고마켓
+              </Link>
+            </li>
+          </ul>
+        </nav>
+      </div>
+
+      <Link to="/login" className="loginLink button">
+        로그인
+      </Link>
+    </header>
+  );
+}
+
+export default Header;
